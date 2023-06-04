063b99485fea74e09ac2af22ae49627013493b1d
skratchdot <git@skratchdot.com>
2023-06-04 19:25:52 -0400

    fix marked after library upgrades

--- src/lib/markdown-to-html.ts
+++ src/lib/markdown-to-html.ts
@@ -1,31 +1,39 @@
 import { fixImages } from './fix-images';
+import { gfmHeadingId } from 'marked-gfm-heading-id';
 import hljs from 'highlight.js';
 import { marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 
 class SkratchdotRenderer extends marked.Renderer {
   code(code: string, infostring: string | undefined, escaped: boolean): string {
     if (infostring === '' || !infostring) {
       infostring = 'plaintext';
     }
-    // @ts-ignore
     return super.code(code, infostring, escaped);
   }
 }
 
-export const markdownToHtml = (markdownContent: string): string => {
-  const html = marked.parse(markdownContent, {
-    renderer: new SkratchdotRenderer(), // new marked.Renderer(),
+marked.use({ async: false, renderer: new SkratchdotRenderer() });
+marked.use(gfmHeadingId());
+marked.use(
+  markedHighlight({
+    async: false,
+    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
     highlight: (code, lang) => {
       const language = hljs.getLanguage(lang) ? lang : 'plaintext';
       const html = hljs.highlight(code, { language }).value;
       return html;
     },
-    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+  })
+);
+
+export const markdownToHtml = (markdownContent: string): string => {
+  const html = marked.parse(markdownContent, {
     pedantic: false,
     gfm: true,
     breaks: false,
+    mangle: false,
     sanitize: false,
-    smartLists: true,
     smartypants: false,
     xhtml: false,
   });
