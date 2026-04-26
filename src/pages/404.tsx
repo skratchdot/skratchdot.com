f67ce4a05a9a26f8898eda581ff8e28cb5a823a1
skratchdot <git@skratchdot.com>
2026-04-26 18:17:12 -0400

    fix variable name

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Page from '../components/Page';
 import { SITE_URL } from '../constants/site';
 
-const whitePageStyles = {
+const errorPageStyles = {
   color: '#fff',
 };
 
@@ -13,8 +13,8 @@ const ErrorPage: NextPage = () => {
       title="Page Not Found"
       canonical={`${SITE_URL}/404`}
     >
-      <h3 style={whitePageStyles}>Error 404 - Not Found</h3>
-      <h4 style={whitePageStyles}>
+      <h3 style={errorPageStyles}>Error 404 - Not Found</h3>
+      <h4 style={errorPageStyles}>
         Sorry, the page that you are looking for does not exist.
       </h4>
     </Page>
