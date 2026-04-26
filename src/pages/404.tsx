375fc9ea1817b48ec41bf7b3bdc79fde99b59127
skratchdot <git@skratchdot.com>
2026-04-26 16:56:04 -0400

    removing css modules in prep for next16 and turbopack

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,20 @@
 import type { NextPage } from 'next';
 import Page from '../components/Page';
 import { SITE_URL } from '../constants/site';
-import styles from './404.module.css';
+
+const whitePageStyles = {
+  color: '#fff',
+};
 
 const ErrorPage: NextPage = () => {
   return (
     <Page
-      pageClassName={styles.ErrorPage}
+      pageClassName="ErrorPage_404"
       title="Page Not Found"
       canonical={`${SITE_URL}/404`}
     >
-      <h3 className={styles.WhiteText}>Error 404 - Not Found</h3>
-      <h4 className={styles.WhiteText}>
+      <h3 style={whitePageStyles}>Error 404 - Not Found</h3>
+      <h4 style={whitePageStyles}>
         Sorry, the page that you are looking for does not exist.
       </h4>
     </Page>
